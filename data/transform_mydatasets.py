1dc45bae544fd4352777b56be94c11928bfa8c40
Cosmina98 <34158786+cosmina98@users.noreply.github.com>
2023-06-26 21:48:23 +0100

    Update transform_mydatasets.py

--- data/transform_mydatasets.py
+++ data/transform_mydatasets.py
@@ -1,8 +1,8 @@
 import json
 
 import numpy as np
-atomic_num_list=[1, 35, 6, 7, 8, 9, 15, 16, 17, 53,0]
-max_atoms = 55
+atomic_num_list=[1, 3, 6, 7, 8, 9, 11, 14, 15, 16, 17, 80, 78, 20, 24, 25, 26, 29, 30, 33, 34, 35, 50, 51, 53,0]
+max_atoms = 133
 n_bonds = 4
 
 
@@ -31,7 +31,7 @@ def get_atomic_num_list(data_name):
     elif data_name == 'ames_40_train1_neg':
          atomic_num_list=[1, 35, 6, 7, 8, 9, 15, 16, 17, 53,0]
     elif data_name == 'ames_40_train1_pos':
-         atomic_num_list=[1, 35, 6, 7, 8, 9, 15, 16, 17, 53],0
+         atomic_num_list=[1, 35, 6, 7, 8, 9, 15, 16, 17, 53,0]
     elif data_name == 'bbb_martins_25_train1_neg':
          atomic_num_list=[1, 35, 6, 7, 8, 9, 11, 15, 16, 17, 53,0]
     elif data_name == 'bbb_martins_25_train1_pos':
@@ -57,13 +57,13 @@ def get_atomic_num_list(data_name):
     elif data_name == 'cyp1a2_veith_33_train1_pos':
          atomic_num_list=[1, 5, 6, 7, 8, 9, 11, 14, 15, 16, 17, 80, 29, 34, 35, 53,0]
     elif data_name == 'cyp1a2_veith_40_train1_neg':
-         atomic_num_list=[1, 3, 6, 7, 8, 9, 11, 78, 15, 16, 17, 80, 19, 14, 24, 25, 26, 27, 30, 33, 34, 35, 51, 53],0
+         atomic_num_list=[1, 3, 6, 7, 8, 9, 11, 78, 15, 16, 17, 80, 19, 14, 24, 25, 26, 27, 30, 33, 34, 35, 51, 53,0]
     elif data_name == 'cyp1a2_veith_40_train1_pos':
          atomic_num_list=[1, 5, 6, 7, 8, 9, 11, 78, 15, 16, 17, 80, 14, 29, 34, 35, 53,0]
     elif data_name == 'cyp1a2_veith_50_train1_neg':
          atomic_num_list=[1, 3, 6, 7, 8, 9, 11, 78, 15, 16, 17, 80, 19, 14, 24, 25, 26, 27, 30, 33, 34, 35, 51, 53,0]
     elif data_name == 'cyp1a2_veith_50_train1_pos':
-         atomic_num_list=[1, 5, 6, 7, 8, 9, 11, 78, 15, 16, 17, 80, 14, 29, 34, 35, 53,0]
+         atomic_num_list=[1, 6, 7, 8, 9, 11, 78, 15, 16, 17, 14, 80, 28, 29, 34, 35, 53,0]
     elif data_name == 'cyp2c19_veith_25_train1_neg':
          atomic_num_list=[1, 3, 5, 6, 7, 8, 9, 11, 14, 15, 16, 17, 80, 78, 20, 26, 29, 33, 35, 50, 51, 53,0]
     elif data_name == 'cyp2c19_veith_25_train1_pos':
@@ -77,7 +77,7 @@ def get_atomic_num_list(data_name):
     elif data_name == 'cyp2c19_veith_40_train1_pos':
          atomic_num_list=[1, 5, 6, 7, 8, 9, 11, 14, 15, 16, 17, 80, 19, 78, 26, 29, 35, 53,0]
     elif data_name == 'cyp2c19_veith_50_train1_neg':
-         atomic_num_list=[1, 5, 6, 7, 8, 9, 11, 78, 15, 16, 17, 14, 19, 20, 25, 26, 29, 30, 33, 34, 35, 44, 50, 51, 53,0]
+         atomic_num_list=[1, 3, 6, 7, 8, 9, 11, 14, 15, 16, 17, 80, 78, 20, 24, 25, 26, 29, 30, 33, 34, 35, 50, 51, 53,0]
     elif data_name == 'cyp2c19_veith_50_train1_pos':
          atomic_num_list=[1, 5, 6, 7, 8, 9, 11, 14, 15, 16, 17, 80, 19, 78, 29, 35, 53,0]
     elif data_name == 'herg_karim_25_train1_neg':
@@ -117,57 +117,57 @@ def get_atomic_num_list(data_name):
 
 def get_max_atoms(data_name):
     if  data_name == 'ames_25_train1_neg':
-        max_atoms=55
+        max_atoms=57
     elif data_name == 'ames_25_train1_pos':
-         max_atoms=54
+         max_atoms=57
     elif data_name == 'ames_33_train1_neg':
-         max_atoms=55
+         max_atoms=57
     elif data_name == 'ames_33_train1_pos':
-         max_atoms=54
+         max_atoms=57
     elif data_name == 'ames_50_train1_neg':
-         max_atoms=55
+         max_atoms=57
     elif data_name == 'ames_50_train1_pos':
-         max_atoms=54
+         max_atoms=57
     elif data_name == 'ames_40_train1_neg':
-         max_atoms=55
+         max_atoms=57
     elif data_name == 'ames_40_train1_pos':
-         max_atoms=54
+         max_atoms=57
     elif data_name == 'bbb_martins_25_train1_neg':
-         max_atoms=123
+         max_atoms=133
     elif data_name == 'bbb_martins_25_train1_pos':
          max_atoms=76
     elif data_name == 'bbb_martins_33_train1_neg':
-         max_atoms=123
+         max_atoms=133
     elif data_name == 'bbb_martins_33_train1_pos':
          max_atoms=76
     elif data_name == 'bbb_martins_50_train1_neg':
-         max_atoms=123
+         max_atoms=133
     elif data_name == 'bbb_martins_50_train1_pos':
          max_atoms=76
     elif data_name == 'bbb_martins_40_train1_neg':
-         max_atoms=123
+         max_atoms=133
     elif data_name == 'bbb_martins_40_train1_pos':
          max_atoms=76
     elif data_name == 'cyp1a2_veith_25_train1_neg':
-         max_atoms=123
+         max_atoms=133
     elif data_name == 'cyp1a2_veith_25_train1_pos':
-         max_atoms=106
+         max_atoms=133
     elif data_name == 'cyp1a2_veith_33_train1_neg':
-         max_atoms=123
+         max_atoms=133
     elif data_name == 'cyp1a2_veith_33_train1_pos':
-         max_atoms=85
+         max_atoms=133
     elif data_name == 'cyp1a2_veith_50_train1_neg':
-         max_atoms=123
+         max_atoms=133
     elif data_name == 'cyp1a2_veith_50_train1_pos':
-         max_atoms=106
+         max_atoms=133
     elif data_name == 'cyp1a2_veith_40_train1_neg':
-         max_atoms=123
+         max_atoms=133
     elif data_name == 'cyp1a2_veith_40_train1_pos':
-         max_atoms=106
+         max_atoms=133
     elif data_name == 'cyp2c19_veith_25_train1_neg':
-         max_atoms=85
+         max_atoms=133
     elif data_name == 'cyp2c19_veith_25_train1_pos':
-         max_atoms=67
+         max_atoms=133
     elif data_name == 'cyp2c19_veith_33_train1_neg':
          max_atoms=101
     elif data_name == 'cyp2c19_veith_33_train1_pos':
